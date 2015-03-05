1c71f4ab15c8b495bdd69801a83a02ce47d45ce5
Alex Van Liew <snoozbuster@outlook.com>
2015-03-04 17:11:52 -0800

    Rewrote sync_html in pure Python (no more rsync!)
    I can't guarantee that this works *exactly* like rsync. It is a lot more
    complex and a lot more nuanced than I can possibly capture in 100 lines,
    even with Python. However, I'm pretty sure it does what you would expect
    in all but the edgiest of cases. For the intention it was being used for,
    it should work just fine.

--- src/markdoc/cli/commands.py
+++ src/markdoc/cli/commands.py
@@ -3,6 +3,7 @@
 import codecs
 from functools import wraps
 import logging
+import fnmatch
 import os
 import os.path as p
 import pprint
@@ -18,41 +19,42 @@ from markdoc.cli.parser import subparsers
 
 def command(function):
     """Decorator/wrapper to declare a function as a Markdoc CLI task."""
-    
+
     cmd_name = function.__name__.replace('_', '-')
     help = (function.__doc__ or '').rstrip('.') or None
     parser = subparsers.add_parser(cmd_name, help=help)
-    
+
     @wraps(function)
     def wrapper(config, args):
         logging.getLogger('markdoc').debug('Running markdoc.%s' % cmd_name)
         return function(config, args)
+
     wrapper.parser = parser
-    
+
     return wrapper
 
 
-## Utilities
+# # Utilities
 
 @command
 def show_config(config, args):
     """Pretty-print the current Markdoc configuration."""
-    
+
     pprint.pprint(config)
 
 
 @command
 def init(_, args):
     """Initialize a new Markdoc repository."""
-    
+
     log = logging.getLogger('markdoc.init')
-    
+
     if not args.destination:
         log.info('No destination specified; using current directory')
         destination = os.getcwd()
     else:
         destination = p.abspath(args.destination)
-    
+
     if p.exists(destination) and os.listdir(destination):
         init.parser.error("destination isn't empty")
     elif not p.exists(destination):
@@ -60,14 +62,14 @@ def init(_, args):
         os.makedirs(destination)
     elif not p.isdir(destination):
         init.parser.error("destination isn't a directory")
-    
+
     log.debug('mkdir %s/.templates/' % destination)
     os.makedirs(p.join(destination, '.templates'))
     log.debug('mkdir %s/static/' % destination)
     os.makedirs(p.join(destination, 'static'))
     log.debug('mkdir %s/wiki/' % destination)
     os.makedirs(p.join(destination, 'wiki'))
-    
+
     log.debug('Creating markdoc.yaml file')
     config_filename = p.join(destination, 'markdoc.yaml')
     fp = open(config_filename, 'w')
@@ -75,36 +77,36 @@ def init(_, args):
         fp.write('{}\n')
     finally:
         fp.close()
-    
+
     if args.vcs_ignore:
         config = markdoc.config.Config.for_directory(destination)
         args = vcs_ignore.parser.parse_args([args.vcs_ignore])
         vcs_ignore(config, args)
-    
+
     log.info('Wiki initialization complete')
     log.info('Your new wiki is at: %s' % destination)
 
 init.parser.add_argument('destination', default=None,
-    help="Create wiki here (if omitted, defaults to current directory)")
+                         help="Create wiki here (if omitted, defaults to current directory)")
 init.parser.add_argument('--vcs-ignore', choices=['hg', 'git', 'cvs', 'bzr'],
-    help="Create an ignore file for the specified VCS.")
+                         help="Create an ignore file for the specified VCS.")
 
 
 @command
 def vcs_ignore(config, args):
     """Create a VCS ignore file for a wiki."""
-    
+
     log = logging.getLogger('markdoc.vcs-ignore')
     log.debug('Creating ignore file for %s' % args.vcs)
-    wiki_root = config['meta.root'] # shorter local alias.
-    
+    wiki_root = config['meta.root']  # shorter local alias.
+
     ignore_file_lines = []
     ignore_file_lines.append(p.relpath(config.html_dir, start=wiki_root))
     ignore_file_lines.append(p.relpath(config.temp_dir, start=wiki_root))
     if args.vcs == 'hg':
         ignore_file_lines.insert(0, 'syntax: glob')
         ignore_file_lines.insert(1, '')
-    
+
     if args.output == '-':
         log.debug('Writing ignore file to stdout')
         fp = sys.stdout
@@ -115,22 +117,22 @@ def vcs_ignore(config, args):
             filename = p.join(wiki_root, args.output)
         log.info('Writing ignore file to %s' % p.relpath(filename, start=wiki_root))
         fp = open(filename, 'w')
-    
+
     try:
         fp.write('\n'.join(ignore_file_lines) + '\n')
     finally:
         if fp is not sys.stdout:
             fp.close()
-    
+
     log.debug('Ignore file written.')
 
 vcs_ignore.parser.add_argument('vcs', default='hg', nargs='?',
-    choices=['hg', 'git', 'cvs', 'bzr'],
-    help="Create ignore file for specified VCS (default 'hg')")
+                               choices=['hg', 'git', 'cvs', 'bzr'],
+                               help="Create ignore file for specified VCS (default 'hg')")
 vcs_ignore.parser.add_argument('-o', '--output', default=None, metavar='FILENAME',
-    help="Write output to the specified filename, relative to the wiki root. "
-         "Default is to generate the filename from the VCS. "
-         "'-' will write to stdout.")
+                               help="Write output to the specified filename, relative to the wiki root. "
+                                    "Default is to generate the filename from the VCS. "
+                                    "'-' will write to stdout.")
 
 
 ## Cleanup
@@ -138,13 +140,13 @@ vcs_ignore.parser.add_argument('-o', '--output', default=None, metavar='FILENAME
 @command
 def clean_html(config, args):
     """Clean built HTML from the HTML root."""
-    
+
     log = logging.getLogger('markdoc.clean-html')
-    
+
     if p.exists(config.html_dir):
         log.debug('rm -Rf %s' % config.html_dir)
         shutil.rmtree(config.html_dir)
-    
+
     log.debug('makedirs %s' % config.html_dir)
     os.makedirs(config.html_dir)
 
@@ -152,13 +154,13 @@ def clean_html(config, args):
 @command
 def clean_temp(config, args):
     """Clean built HTML from the temporary directory."""
-    
+
     log = logging.getLogger('markdoc.clean-temp')
-    
+
     if p.exists(config.temp_dir):
         log.debug('rm -Rf %s' % config.temp_dir)
         shutil.rmtree(config.temp_dir)
-    
+
     log.debug('makedirs %s' % config.temp_dir)
     os.makedirs(config.temp_dir)
 
@@ -168,76 +170,161 @@ def clean_temp(config, args):
 @command
 def sync_static(config, args):
     """Sync static files into the HTML root."""
-    
+
     log = logging.getLogger('markdoc.sync-static')
-    
+
     if not p.exists(config.html_dir):
         log.debug('makedirs %s' % config.html_dir)
         os.makedirs(config.html_dir)
-    
+
     command = ('rsync -vaxq --cvs-exclude --ignore-errors --include=.htaccess --exclude=.* --exclude=_*').split()
     display_cmd = command[:]
-    
+
     if config['use-default-static']:
         # rsync needs the paths to have trailing slashes to work correctly.
         command.append(p.join(markdoc.default_static_dir, ''))
         display_cmd.append(p.basename(markdoc.default_static_dir) + '/')
-    
+
     if not config['cvs-exclude']:
         command.remove('--cvs-exclude')
         display_cmd.remove('--cvs-exclude')
-    
+
     if p.isdir(config.static_dir):
         command.append(p.join(config.static_dir, ''))
         display_cmd.append(p.basename(config.static_dir) + '/')
-    
+
     command.append(p.join(config.html_dir, ''))
     display_cmd.append(p.basename(config.html_dir) + '/')
-    
+
     log.debug(subprocess.list2cmdline(display_cmd))
-    
+
     subprocess.check_call(command)
-    
+
     log.debug('rsync completed')
 
 
 @command
 def sync_html(config, args):
     """Sync built HTML and static media into the HTML root."""
-    
+
+    # the previous command used here was:
+    # rsync -vaxq [--cvs-exclude] --delete --ignore-errors --include=.htaccess --exclude=.* --exclude=_*
+    # it is unclear why both verbose and quiet were specified.
+
     log = logging.getLogger('markdoc.sync-html')
-    
+
+    exp = r'\..*|\_.*'
+
+    if config['cvs-exclude']:
+        # simulate --cvs-exclude (behavior from rsync man page)
+        # use a couple groups to make the regex a little shorter
+        cvsexp = (r'RCS|SCCS|CVS(?:\.adm)?|RCSLOG|cvslog\..*|tags|TAGS|\.make\.state|\.nse_depinfo|.*~|'
+                  r'#.*|\.#.*|,.*|_\$.*|.*\$|.*\.(?:bak|BAK)|.*\.orig|.*\.rej|\.del-.*|.*\.a|core|\.svn/|'
+                  r'.*\.o(?:bj|lb|ld)?|.*\.so|.*\.e(?:xe|lc)|.*\.Z|.*\.ln|\.git/|\.hg/|\.bzr/')
+        exp += "|" + cvsexp
+        try:
+            with open(os.path.expanduser(".cvsignore")) as cvs:
+                cvsignore = cvs.readall().replace('\n', ' ').split()
+                # convert fs globs to regexes and tack them on the end
+                for glob in cvsignore:
+                    exp += "|" + fnmatch.translate(glob)
+        except IOError:
+            log.debug("could not read ~/.cvsignore (might not exist)")
+
+        cvsenv = os.environ.get("CVSIGNORE", "").split()
+        for glob in cvsenv:
+            exp += "|" + fnmatch.translate(glob)
+
+    log.debug("Regex: {0}".format(exp))
+    exp = re.compile(exp)
+
     if not p.exists(config.html_dir):
         log.debug('makedirs %s' % config.html_dir)
         os.makedirs(config.html_dir)
-    
-    command = ('rsync -vaxq --cvs-exclude --delete --ignore-errors --include=.htaccess --exclude=.* --exclude=_*').split()
-    display_cmd = command[:]
-    
-    # rsync needs the paths to have trailing slashes to work correctly.
-    command.append(p.join(config.temp_dir, ''))
-    display_cmd.append(p.basename(config.temp_dir) + '/')
-    
+
+    html_dir = p.join(config.html_dir, '')
+
+    # syncs a dir to the html dir
+    # can't delete without knowing full merged structure of all three paths,
+    # so return a structure of the filesystem copied by this call
+    def rsync(where_from):
+        copied_files = []
+        # walk the dir and sync it
+        for dirname, subdirnames, filenames in os.walk(p.join(where_from, '')):
+            # filter directories
+            to_remove = []
+            for subdirname in subdirnames:
+                subdirname = p.join(p.basename(subdirname), '') # add trailing slash
+                if exp.match(subdirname):
+                    to_remove.append(p.basename(subdirname)) # remove slash
+            # remove directories from listing
+            for subdir in to_remove:
+                subdirnames.remove(subdir)
+
+            # copy files
+            for filename in filenames:
+                # .htaccess is an exception
+                if not exp.match(p.basename(filename)) or p.basename(filename) == ".htaccess":
+                    try:
+                        target = p.join(html_dir, dirname)
+                        copied_files.append(p.join(target, filename))
+                        # attempt to preserve permissions and other attributes as well as symlinks
+                        shutil.copy2(p.join(dirname, filename), target)
+                    except (IOError, OSError) as e:
+                        # this is the --ignore-errors piece, I think
+                        log.warning("file copy failed: {0}".format(e))
+        return copied_files
+
+    filesystem = rsync(p.join(config.tmp_dir, ''))
     if config['use-default-static']:
-        command.append(p.join(markdoc.default_static_dir, ''))
-        display_cmd.append(p.basename(markdoc.default_static_dir) + '/')
-    
-    if not config['cvs-exclude']:
-        command.remove('--cvs-exclude')
-        display_cmd.remove('--cvs-exclude')
-    
+        filesystem += rsync(p.join(markdoc.default_static_dir, ''))
     if p.isdir(config.static_dir):
-        command.append(p.join(config.static_dir, ''))
-        display_cmd.append(p.basename(config.static_dir) + '/')
-    
-    command.append(p.join(config.html_dir, ''))
-    display_cmd.append(p.basename(config.html_dir) + '/')
-    
-    log.debug(subprocess.list2cmdline(display_cmd))
-    
-    subprocess.check_call(command)
-    
-    log.debug('rsync completed')
+        filesystem += rsync(p.join(config.static_dir, ''))
+
+    # munge files to get rid of the /dir/./other_dir that shows up
+    filesystem = [s.replace('./', '') for s in filesystem]
+
+    # delete files
+    # ignore (but log) errors to comply with the old command's --ignore-errors flag
+    for dirname, subdirnames, filenames in os.walk(html_dir):
+        # start with directories
+        deleted = []
+        for subdir in subdirnames:
+            full_subdir = p.join(dirname, subdir)
+            if full_subdir not in filesystem:
+                deleted.append(subdir)
+                log.debug("recursively deleting directory {0}".format(full_subdir))
+                # delete directory
+                for root, dirs, files in os.walk(full_subdir, topdown=False):
+                    for name in files:
+                        try:
+                            os.remove(os.path.join(root, name))
+                        except (IOError, OSError) as e:
+                            log.warning("file delete failed: {0}".format(e))
+                    for name in dirs:
+                        try:
+                            os.rmdir(os.path.join(root, name))
+                        except (IOError, OSError) as e:
+                            log.warning("directory delete failed: {0}".format(e))
+                try:
+                    os.rmdir(full_subdir)
+                except (IOError, OSError) as e:
+                    log.warning("directory delete failed: {0}".format(e))
+        # don't walk deleted directories
+        for subdir in deleted:
+            subdirnames.remove(subdir)
+
+        # delete files
+        for filename in filenames:
+            full_name = p.join(dirname, filename)
+            if full_name not in filesystem:
+                log.debug("deleting file {0}".format(full_name))
+                try:
+                    os.remove(full_name)
+                except (IOError, OSError) as e:
+                    log.warning("file delete failed: {0}".format(e))
+
+    log.debug('sync complete')
 
 
 ## Building
@@ -245,28 +332,28 @@ def sync_html(config, args):
 @command
 def build(config, args):
     """Compile wiki to HTML and sync to the HTML root."""
-    
+
     log = logging.getLogger('markdoc.build')
-    
+
     clean_temp(config, args)
-    
+
     builder = Builder(config)
     for rel_filename in builder.walk():
         html = builder.render_document(rel_filename)
         out_filename = p.join(config.temp_dir,
-            p.splitext(rel_filename)[0] + p.extsep + 'html')
-        
+                              p.splitext(rel_filename)[0] + p.extsep + 'html')
+
         if not p.exists(p.dirname(out_filename)):
             log.debug('makedirs %s' % p.dirname(out_filename))
             os.makedirs(p.dirname(out_filename))
-        
+
         log.debug('Creating %s' % p.relpath(out_filename, start=config.temp_dir))
         fp = codecs.open(out_filename, 'w', encoding='utf-8')
         try:
             fp.write(html)
         finally:
             fp.close()
-    
+
     sync_html(config, args)
     build_listing(config, args)
 
@@ -274,40 +361,40 @@ def build(config, args):
 @command
 def build_listing(config, args):
     """Create listings for all directories in the HTML root (post-build)."""
-    
+
     log = logging.getLogger('markdoc.build-listing')
-    
+
     list_basename = config['listing-filename']
     builder = Builder(config)
     generate_listing = config.get('generate-listing', 'always').lower()
     always_list = True
     if generate_listing == 'never':
         log.debug("No listing generated (generate-listing == never)")
-        return # No need to continue.
-    
+        return  # No need to continue.
+
     for fs_dir, _, _ in os.walk(config.html_dir):
         index_file_exists = any([
             p.exists(p.join(fs_dir, 'index.html')),
             p.exists(p.join(fs_dir, 'index'))])
-        
+
         directory = '/' + '/'.join(p.relpath(fs_dir, start=config.html_dir).split(p.sep))
         if directory == '/' + p.curdir:
             directory = '/'
-        
+
         if (generate_listing == 'sometimes') and index_file_exists:
             log.debug("No listing generated for %s" % directory)
             continue
-        
+
         log.debug("Generating listing for %s" % directory)
         listing = builder.render_listing(directory)
         list_filename = p.join(fs_dir, list_basename)
-        
+
         fp = codecs.open(list_filename, 'w', encoding='utf-8')
         try:
             fp.write(listing)
         finally:
             fp.close()
-        
+
         if not index_file_exists:
             log.debug("cp %s/%s %s/%s" % (directory, list_basename, directory, 'index.html'))
             shutil.copyfile(list_filename, p.join(fs_dir, 'index.html'))
@@ -320,13 +407,13 @@ IPV4_RE = re.compile(r'^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]
 @command
 def serve(config, args):
     """Serve the built HTML from the HTML root."""
-    
+
     # This should be a lazy import, otherwise it'll slow down the whole CLI.
     from markdoc.wsgi import MarkdocWSGIApplication
-    
+
     log = logging.getLogger('markdoc.serve')
     app = MarkdocWSGIApplication(config)
-    
+
     config['server.port'] = args.port
     config['server.num-threads'] = args.num_threads
     if args.server_name:
@@ -337,9 +424,9 @@ def serve(config, args):
         if not IPV4_RE.match(args.interface):
             serve.parser.error('invalid interface specifier: %r' % args.interface)
         config['server.bind'] = args.interface
-    
+
     server = config.server_maker()(app)
-    
+
     try:
         log.info('Serving on http://%s:%d' % server.bind_addr)
         server.start()
@@ -350,15 +437,15 @@ def serve(config, args):
         server.stop()
 
 serve.parser.add_argument('-p', '--port', type=int, default=8008,
-    help="Listen on specified port (default is 8008)")
+                          help="Listen on specified port (default is 8008)")
 serve.parser.add_argument('-i', '--interface', default=None,
-    help="Bind to specified interface (defaults to loopback only)")
+                          help="Bind to specified interface (defaults to loopback only)")
 serve.parser.add_argument('-t', '--num-threads', type=int, default=10, metavar='N',
-    help="Use N threads to handle requests (default is 10)")
+                          help="Use N threads to handle requests (default is 10)")
 serve.parser.add_argument('-n', '--server-name', default=None, metavar='NAME',
-    help="Use an explicit server name (default to an autodetected value)")
+                          help="Use an explicit server name (default to an autodetected value)")
 serve.parser.add_argument('-q', '--queue-size', type=int, default=5, metavar='SIZE',
-    help="Set request queue size (default is 5)")
+                          help="Set request queue size (default is 5)")
 serve.parser.add_argument('--timeout', type=int, default=10,
-    help="Set the socket timeout for connections (default is 10)")
+                          help="Set the socket timeout for connections (default is 10)")
 
